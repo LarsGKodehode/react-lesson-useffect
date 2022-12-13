dcbbe5b397e2128ef1db0b27c1b87a9dd1c386c7
LarsG <lars.ga.solheim@gmail.com>
2022-12-13 15:33:12 +0100

    extracted fetch to a seperate function

--- src/commponents/FetchData.jsx
+++ src/commponents/FetchData.jsx
@@ -1,18 +1,30 @@
 import { useState, useEffect } from "react";
 
 /**
+ * Create the fetch data function here
+ */
+async function getData(url, callback) {
+  const response = await fetch(url)
+  const data = await response.json();
+
+  callback(data.results)
+}
+
+
+/**
  * Fetches data from some API and displays it
  */
 function FetchData() {
   // Somewhere to store our data
   const [ fetchedData, setFetchedData ] = useState(null)
+  const [ url, setUrl ] = useState("https://pokeapi.co/api/v2/pokemon")
 
   // Using .then for asynchronous
   // useEffect(
   //   () => {
   //     fetch("https://pokeapi.co/api/v2/pokemon")
-  //       .then(respons => respons.json())
-  //       .then(data => setFetchedData(data.results))
+  //       .then((respons) => {respons.json()})
+  //       .then((data) => {setFetchedData(data.results)})
 
   //     return () => {
   //       // Code in here get run when this component gets removed from the DOM
@@ -24,29 +36,26 @@ function FetchData() {
   // Using async await
   useEffect(
     () => {
-      async function getData() {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon")
-        const data = await response.json();
-
-        setFetchedData(data.results)
-      }
-
-      getData()
+      getData(url, setFetchedData)
     },
     []
   )
   
 
   return (
-    <ul>
-      {
-      fetchedData &&
-      fetchedData.map((pokemon, index) => {
-        return (
-          <li key={index}>{pokemon.name}</li>
-        )
-      })}
-    </ul>
+    <>
+      <button onClick={() => getData(url, setFetchedData)}>Get New Data</button>
+
+      <ul>
+        {
+        fetchedData &&
+        fetchedData.map((pokemon, index) => {
+          return (
+            <li key={index}>{pokemon.name}</li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
